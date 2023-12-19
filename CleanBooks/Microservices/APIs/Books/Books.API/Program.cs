9a2f3f751ba1a66cb1691e89a069b3bfb540fa13
MDNET22 <mathias.damgaard@iths.se>
2023-12-19 12:10:50 +0100

    Changed from Postgres to mongodb

--- CleanBooks/Microservices/APIs/Books/Books.API/Program.cs
+++ CleanBooks/Microservices/APIs/Books/Books.API/Program.cs
@@ -1,8 +1,6 @@
-using Books.DataAccess.Contexts;
 using Books.DataAccess.Repositories;
 using Books.DataAccess.Repositories.Interfaces;
 using FastEndpoints;
-using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,16 +8,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddFastEndpoints();
-builder.Services.AddSwaggerGen();
-
-
-builder.Services.AddDbContext<BooksDbContext>(options =>
-{
-    options.UseNpgsql(builder.Configuration.GetConnectionString("BooksDb"));
-});
-
 builder.Services.AddScoped<IBookRepository, BookRepository>();
-
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
