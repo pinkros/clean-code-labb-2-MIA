6b829d7099e28c8f3386222b47c0884346161996
Ida Borén <ida.boren@iths.se>
2023-12-20 09:47:58 +0100

    dbcontext fix

--- CleanBooks/Microservices/APIs/Users/Users.API/Program.cs
+++ CleanBooks/Microservices/APIs/Users/Users.API/Program.cs
@@ -20,7 +20,8 @@ var host = Environment.GetEnvironmentVariable("DB_HOST");
 var database = Environment.GetEnvironmentVariable("DB_DATABASE");
 var username = Environment.GetEnvironmentVariable("DB_USER");
 var password = Environment.GetEnvironmentVariable("DB_MSSQL_SA_PASSWORD");
-var connectionString = $"Data Source={host};Initial Catalog={database};User ID={username};Password={password};Trusted_connection=False;TrustServerCertificate=True;";
+var connectionString =
+    $"Data Source={host};Initial Catalog={database};User ID={username};Password={password};Trusted_connection=False;TrustServerCertificate=True;";
 
 builder.Services.AddSqlServer<UsersDbContext>(connectionString);
 
--- CleanBooks/Microservices/APIs/Users/Users.DataAccess/Contexts/UsersDbContext.cs
+++ CleanBooks/Microservices/APIs/Users/Users.DataAccess/Contexts/UsersDbContext.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
 using Users.DataAccess.Models;
 
 namespace Users.DataAccess.Contexts;
@@ -9,6 +11,17 @@ public class UsersDbContext : DbContext
 
     public UsersDbContext(DbContextOptions<UsersDbContext> options) : base(options)
     {
-
+        try
+        {
+            if (Database.GetService<IDatabaseCreator>() is RelationalDatabaseCreator databaseCreator)
+            {
+                if (!databaseCreator.CanConnect()) databaseCreator.Create();
+                if (!databaseCreator.HasTables()) databaseCreator.CreateTables();
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
     }
 }
